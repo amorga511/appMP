6015d03d144abe5b8ef4a0c157c782c72df2ac35
amorga511 <amorga511@gmail.com>
2017-05-08 23:01:16 -0600

    Add files via upload

--- js/js_main.js
+++ js/js_main.js
@@ -2,15 +2,10 @@
 
 $(document).ready(function(e){
 
-    navigator.splashscreen.show();
-    setTimeout(function () {
-        navigator.splashscreen.hide();
-    }, 6000);
-
-    /*$("#dv_splash").show();
+    $("#dv_splash").show();
     $("#dvHead").hide();
     setTimeout(function(){ 
         $("#dv_splash").hide(); 
         $("#dvHead").show();
-    }, 3000);*/
+    }, 3000);
 });
